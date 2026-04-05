77faa0c672819c01c089a988dc429b8c9cb688e3
Joel <joelndala356@gmail.com>
2026-04-05 15:52:50 +0100

    Fim

--- script.js
+++ script.js
@@ -4,36 +4,52 @@ const author = document.querySelector('#author')
 const Frase = document.querySelector('#quote')
 const tag0 = document.querySelector('#tag0')
 const tag1 = document.querySelector('#tag1')
+let CurrentQuote = {}
 
+async function DisplayRandomQuote() {
 
-let Quote = []
-function DisplayRandomQuote(){
-    fetch('https://raw.githubusercontent.com/devchallenges-io/curriculum/refs/heads/main/3-javascript/challenges/group_1/data/random-quotes.json')
-    .then(resp => resp.json())
-    .then(dados =>{
-        let randomIndex = Math.floor(Math.random() * dados.length);
-        let dado = dados[randomIndex]
+    
+    try{
+        const resp = await fetch('https://raw.githubusercontent.com/devchallenges-io/curriculum/refs/heads/main/3-javascript/challenges/group_1/data/random-quotes.json')
         
-        Quote.push(dado.author)
-        Quote.push(dado.quote)
+        const dados = await resp.json()
+        const randomIndex = Math.floor(Math.random() * dados.length);
+
+        const dado = dados[randomIndex];
         author.innerHTML = dado.author
         Frase.innerHTML = `"${dado.quote}"`
         tag0.innerHTML = dado.tags[0]
         tag1.innerHTML = dado.tags[1]
-    })
-    .catch(err => {
+
+        CurrentQuote = {
+            autor: dado.author,
+            frase: dado.quote
+        }
+        
+
+    }catch (err){
         alert('Não foi possível carregar as citações. 😕\nVerifique sua conexão com a internet.');
-    })
+    }
+    
 }
+
 
 DisplayRandomQuote()
 randonBtn.addEventListener('click', DisplayRandomQuote);
 
 
-console.log(Quote)
-async function copiarParaClipboard(Quote) {
+
+async function copiarParaClipboard() {
+    if(!CurrentQuote.frase){
+        alert("Clique no botão \"random\" Primeiro")
+    }
+    const textoParaCopiar = `"${CurrentQuote.frase}"\n\n— ${CurrentQuote.autor}`
     try {
-        await navigator.clipboard.writeText(Quote);
+        // 
+        await navigator.clipboard.writeText(textoParaCopiar);
+
+        console.log( textoParaCopiar)
+        
         alert('✅ Texto copiado com sucesso!');
     } catch (erro) {
         console.error('Erro ao copiar:', erro);
